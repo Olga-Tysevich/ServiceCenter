311d39b0b830337dc555133504e7bfeb76cddd0c
Olga Tysevich <Olga.Tisevich@gmail.com>
2024-06-15 17:11:51 +0300

    fix order

--- src/main/resources/static/js/SparePartOrderBehavior.js
+++ src/main/resources/static/js/SparePartOrderBehavior.js
@@ -1,31 +1,50 @@
+const messageBlock = $('#message-block-id');
+const orderBlock = $('#order-body-id');
+
 $(document).ready(function () {
     $('#add-id').click(function () {
+        messageBlock.hide();
         let selectedPart = $('#spare-parts-container-id option:selected');
         let selectedPartId = selectedPart.val();
         let selectedPartName = selectedPart.text();
         let quantity = $('#quantity-id').val();
 
+        if (quantity <= 0) {
+            $('#message-id').text('Количество не может быть меньше 1!')
+            messageBlock.css('color', 'RED');
+            messageBlock.show();
+            return;
+        }
+
+        let test = orderBlock.find(`td:contains(${selectedPartName})`);
+        if (test.length !== 0) {
+            let quantityTemp = test.closest('td').closest('tr').find('.spare_part_quantity').text();
+            let newQuantity = parseInt(quantityTemp) + parseInt(quantity);
+            test.closest('tr').find('.spare_part_quantity').text(newQuantity);
+            return;
+        }
+
         let newRow = `
             <tr class="order-item">
-                <td>${selectedPartName}</td>
-                <td>${quantity}</td>
+                <td class="spare_part_name">${selectedPartName}</td>
+                <td class="spare_part_quantity">${quantity}</td>
                 <td style="display: none" class="spare-part-id-container">${selectedPartId}</td>
                 <td>
                     <input class="delete_btn" type="button" value="-" id="delete-btn-id"/>
                 </td>
             </tr>
             `;
-        $('#order-body-id').append(newRow);
+        orderBlock.append(newRow);
     });
 
-    $('#order-body-id').on('click', '#delete-btn-id', function () {
+    orderBlock.on('click', '#delete-btn-id', function () {
         $(this).closest('tr').remove();
     });
 });
 
 function closeOrderForm() {
     let orderForm = $('#order-block-id');
-    $('#message-block-id').show();
+    messageBlock.show();
     orderForm.hide();
 }
 
@@ -58,8 +77,7 @@ function createOrder() {
         contentType: 'application/json',
         data: JSON.stringify(data),
         success: function (response) {
-            let orderItems = $('#order-body-id');
-            orderItems.find('tr').each(function () {
+            orderBlock.find('tr').each(function () {
                 $(this).remove();
             });
             window.location.href = '/spare-part-orders/show-repair-orders/' + repairId + '?repairNumber=' + repairNumber;
@@ -69,7 +87,7 @@ function createOrder() {
                 let errorBlock = $('#message-id');
                 errorBlock.text(xhr.responseText);
                 errorBlock.css('color', 'RED');
-                $('#message-block-id').show();
+                messageBlock.show();
             } else {
                 let errorBlock = $('#message-id');
                 errorBlock.text("Ошибка сервера, попробуйте позднее");
